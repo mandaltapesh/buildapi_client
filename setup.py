8ad473033a7087c6ec0955b463ed2c76578da9d5
Armen Zambrano G <armenzg@mozilla.com>
2016-01-18 09:06:58 -0500

    Back to development: 0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='buildapi_client',
-    version='0.2.2',
+    version='0.2.3.dev0',
     packages=find_packages(),
 
     install_requires=[
